f4ca005bfa9bc1d7b9b70d28a6fde15659b87118
AnatoliyZaginai <arkey7724@gmail.com>
2021-02-14 22:31:52 +0300

    Added Task 1.1

---
+++ Homeworks/Homework_1/Task_1/SortingTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_1
+{
+    class SortingTests
+    {
+        private static bool IsSortedTest()
+        {
+            int[] firstArray = { -1, 2, 2, 3, 4 };
+            int[] secondArray = { -1, 2, 4, 3, 3 };
+            return Sorting.IsSorted(firstArray) && !Sorting.IsSorted(secondArray);
+        }
+
+        private static bool SortTest()
+        {
+            int[] firstArray = { -1, 2, 2, 3, 4 };
+            Sorting.Sort(firstArray);
+            int[] secondArray = { 3, 1, 2, 0, 4 };
+            Sorting.Sort(secondArray);
+            int[] thirdArray = { -3, -8, 0, 1, -1 };
+            Sorting.Sort(thirdArray);
+            return Sorting.IsSorted(firstArray) && Sorting.IsSorted(secondArray) && Sorting.IsSorted(thirdArray);
+        }
+
+        public static bool Tests()
+        {
+            return IsSortedTest() && SortTest();
+        }
+    }
+}
