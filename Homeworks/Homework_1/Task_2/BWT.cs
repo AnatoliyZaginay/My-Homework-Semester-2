dd805169a7e8d762d396cb4e5ec8328fb4a61ef3
AnatoliyZaginai <arkey7724@gmail.com>
2021-02-19 21:10:58 +0300

    Added BWT Transform

---
+++ Homeworks/Homework_1/Task_2/BWT.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Task_2
+{
+    class BWT
+    {
+        private static string GetCircularShift(string line, int index)
+            => line.Substring(index, line.Length - index) + line.Substring(0, index);
+
+        private static void Sort(int[] array, string line, int firstIndex, int secondIndex)
+        {
+            var left = firstIndex;
+            var right = secondIndex;
+            var pivot = GetCircularShift(line, array[(firstIndex + secondIndex) / 2]);
+            while (left <= right)
+            {
+                while (String.Compare(GetCircularShift(line, array[left]), pivot) < 0)
+                {
+                    ++left;
+                }
+                while (String.Compare(GetCircularShift(line, array[right]), pivot) > 0)
+                {
+                    --right;
+                }
+                if (left <= right)
+                {
+                    var swap = array[left];
+                    array[left] = array[right];
+                    array[right] = swap;
+                    ++left;
+                    --right;
+                }
+                if (right > firstIndex)
+                {
+                    Sort(array, line, firstIndex, right);
+                }
+                if (left < secondIndex)
+                {
+                    Sort(array, line, left, secondIndex);
+                }
+            }
+        }
+
+        public static string Transform(string line)
+        {
+            var circularShiftsArray = new int[line.Length];
+            for (int i = 0; i < line.Length; ++i)
+            {
+                circularShiftsArray[i] = i;
+            }
+            Sort(circularShiftsArray, line, 0, circularShiftsArray.Length - 1);
+            string result = "";
+            for (int i = 0; i < circularShiftsArray.Length; ++i)
+            {
+                result += line[(circularShiftsArray[i] + line.Length - 1) % line.Length];
+            }
+            return result;
+        }
+    }
+}
---
+++ Homeworks/Homework_1/Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select an operation: \n1 - BWT\n2 - Reverse BWT");
+            string line = Console.ReadLine();
+            Console.WriteLine(BWT.Transform(line));
+        }
+    }
+}
