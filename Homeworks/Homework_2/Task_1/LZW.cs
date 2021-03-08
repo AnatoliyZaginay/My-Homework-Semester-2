b0c75889057316c663f84d0c2ecd50e5273da292
AnatoliyZaginai <arkey7724@gmail.com>
2021-03-08 04:54:45 +0300

    Added Trie and compression method

---
+++ Homeworks/Homework_2/Task_1/LZW.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using static System.Math;
+
+namespace Task_1
+{
+    static class LZW
+    {
+        private static int GetCountOfBytes(int number)
+            => (int)Ceiling(Log2(number + 1) / 8);
+
+        public static void Compress(string path)
+        {
+            var trie = new Trie();
+            using FileStream sourceFile = File.OpenRead(path);
+            var compressedFilePath = path + ".zipped";
+            using FileStream compressedFile = File.OpenWrite(compressedFilePath);
+            for (int i = 0; i < sourceFile.Length; ++i)
+            {
+                var currentByte = (byte)sourceFile.ReadByte();
+                if (trie.PointerContains(currentByte))
+                {
+                    trie.MovePointer(currentByte);
+                }
+                else
+                {
+                    var byteCode = BitConverter.GetBytes(trie.PointerCode);
+                    Array.Resize(ref byteCode, GetCountOfBytes(trie.CurrentCode));
+                    compressedFile.Write(byteCode);
+                    trie.Add(currentByte);
+                }
+            }
+            var lastByte = BitConverter.GetBytes(trie.PointerCode);
+            Array.Resize(ref lastByte, GetCountOfBytes(trie.CurrentCode));
+            compressedFile.Write(lastByte);
+        }
+    }
+}
---
+++ Homeworks/Homework_2/Task_1/Trie.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_1
+{
+    class Trie
+    {
+        private class TrieElement
+        {
+            public int Code { get; set; }
+            private Dictionary<byte, TrieElement> childrens;
+
+            public TrieElement()
+                => childrens = new Dictionary<byte, TrieElement>();
+
+            public TrieElement GetChild(byte key)
+                => childrens[key];
+
+            public void AddElement(byte key, int code)
+            {
+                childrens.Add(key, new TrieElement());
+                childrens[key].Code = code;
+            }
+
+            public bool Contains(byte key)
+                => childrens.ContainsKey(key);
+        }
+
+        private TrieElement root;
+        private TrieElement pointer;
+        public int CurrentCode { get; set; }
+
+        public Trie()
+        {
+            root = new TrieElement();
+            for (int i = 0; i < 256; ++i)
+            {
+                root.AddElement((byte)i, i);
+            }
+            pointer = root;
+            CurrentCode = byte.MaxValue;
+        }
+
+        public int PointerCode => pointer.Code;
+
+        private void MovePointerToRoot()
+            => pointer = root;
+
+        public void MovePointer(byte key)
+            => pointer = pointer.GetChild(key);
+
+        public bool PointerContains(byte key)
+            => pointer.Contains(key);
+
+        public void Add(byte key)
+        {
+            ++CurrentCode;
+            pointer.AddElement(key, CurrentCode);
+            MovePointerToRoot();
+            MovePointer(key);
+        }
+    }
+}
