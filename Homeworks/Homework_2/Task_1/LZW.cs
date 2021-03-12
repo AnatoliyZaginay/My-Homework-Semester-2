ca48c858ec5ac848f25510a06ec0c93a55bb77f0
AnatoliyZaginai <arkey7724@gmail.com>
2021-03-12 04:08:34 +0300

    Added comments, some code changes.

--- Homeworks/Homework_2/Task_1/LZW.cs
+++ Homeworks/Homework_2/Task_1/LZW.cs
@@ -5,18 +5,34 @@ using static System.Math;
 
 namespace Task_1
 {
+    /// <summary>
+    /// Static class used for compressing and decompressing files using LZW algorithm.
+    /// </summary>
     public static class LZW
     {
+        /// <summary>
+        /// Returns the number of bytes required to store the specified number.
+        /// </summary>
+        /// <param name="number">Specified number.</param>
         private static int GetCountOfBytes(int number)
-            => (int)Ceiling(Log2(number + 1) / 8);
+            => (int)Ceiling(Log2(number) / 8);
 
+        /// <summary>
+        /// Gets byte representation of the pointer code and write it to the file.
+        /// </summary>
+        /// <param name="trie">The trie where the pointer code is read from.</param>
+        /// <param name="file">Specified file.</param>
         private static void WriteByteCode(Trie trie, FileStream file)
         {
             var byteCode = BitConverter.GetBytes(trie.PointerCode);
-            Array.Resize(ref byteCode, GetCountOfBytes(trie.CurrentCode));
+            Array.Resize(ref byteCode, GetCountOfBytes(trie.CurrentCode + 1));
             file.Write(byteCode);
         }
 
+        /// <summary>
+        /// Compresses the specified file and adds the extension ".zipped".
+        /// </summary>
+        /// <param name="path">Path to the file.</param>
         public static void Compress(string path)
         {
             var trie = new Trie();
@@ -39,6 +55,10 @@ namespace Task_1
             WriteByteCode(trie, compressedFile);
         }
 
+        /// <summary>
+        /// Initializes the byte array dictionary with the first 256 bytes.
+        /// </summary>
+        /// <param name="dictionary">Specified byte array dictionary.</param>
         private static void DictionaryInitialization(Dictionary<int, byte[]> dictionary)
         {
             for (int i = 0; i < 256; ++i)
@@ -48,6 +68,10 @@ namespace Task_1
             }
         }
 
+        /// <summary>
+        /// Returns an integer representation of an array of bytes.
+        /// </summary>
+        /// <param name="byteArray">Specified byte array.</param>
         private static int GetIntCode(byte[] byteArray)
         {
             var arrayForCode = new byte[4];
@@ -55,6 +79,11 @@ namespace Task_1
             return BitConverter.ToInt32(arrayForCode, 0);
         }
 
+        /// <summary>
+        /// Returns a new byte array by adding first element of the second array to the end of the first array.
+        /// </summary>
+        /// <param name="firstArray">First byte array.</param>
+        /// <param name="secondArray">Second byte array.</param>
         private static byte[] GetNewByteArray(byte[] firstArray, byte[] secondArray)
         {
             var newByteArray = new byte[firstArray.Length + 1];
@@ -63,6 +92,10 @@ namespace Task_1
             return newByteArray;
         }
 
+        /// <summary>
+        /// Decompresses the specified file and removes the extension ".zipped".
+        /// </summary>
+        /// <param name="path">Path to the file.</param>
         static public void Decompress(string path)
         {
             using FileStream sourceFile = File.OpenRead(path);
@@ -74,7 +107,7 @@ namespace Task_1
             var position = 0;
             while (position < sourceFile.Length)
             {
-                var currentLength = GetCountOfBytes(dictionary.Count - 1);
+                var currentLength = GetCountOfBytes(dictionary.Count);
                 position += currentLength;
 
                 var currentBytes = new byte[currentLength];
