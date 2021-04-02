be8ff2c3643767a63125896b88552dae533a7032
AnatoliyZaginai <arkey7724@gmail.com>
2021-04-02 12:52:58 +0300

    Added Task_1

---
+++ Tests/Test_1/Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            dict.Add(1, 12);
+            dict.Add(3, 14);
+        }
+    }
+}
