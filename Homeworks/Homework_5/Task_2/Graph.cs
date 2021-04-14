df83ff20c9c6a8c2d91d7954131f82440f0b1bfd
AnatoliyZaginai <arkey7724@gmail.com>
2021-04-14 05:20:05 +0300

    Added graph class and GetSpanningTree method.

---
+++ Homeworks/Homework_5/Task_2/Graph.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_2
+{
+    public class Graph
+    {
+        public int[,] MatrixOfLengths { get; }
+
+        public bool[,] MatrixOfConnections { get; }
+
+        public int CountOfVertexes { get; }
+
+        public Graph(int[,] matrixOfLengths, bool[,] matrixOfConnetions)
+        {
+            if (matrixOfConnetions == null || matrixOfLengths == null || matrixOfConnetions.Length != matrixOfLengths.Length ||
+                matrixOfConnetions.GetLength(0) != matrixOfConnetions.GetLength(1) || matrixOfLengths.GetLength(0) != matrixOfLengths.GetLength(1))
+            {
+                throw new ArgumentException();
+            }
+            MatrixOfLengths = matrixOfLengths;
+            MatrixOfConnections = matrixOfConnetions;
+            CountOfVertexes = matrixOfLengths.GetLength(0);
+        }
+
+        private void DFS(int numberOfVertex, bool[] visited)
+        {
+            for (int i = 0; i < CountOfVertexes; ++i)
+            {
+                if (MatrixOfConnections[numberOfVertex, i] && !visited[i])
+                {
+                    visited[i] = true;
+                    DFS(i, visited);
+                }
+            }
+        }
+
+        public bool IsConnected()
+        {
+            var visited = new bool[CountOfVertexes];
+            DFS(0, visited);
+
+            for (int i = 0; i < CountOfVertexes; ++i)
+            {
+                if (!visited[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private class Edge
+        {
+            public int FirstVertex { get; set; }
+
+            public int SecondVertex { get; set; }
+
+            public int Weight { get; set; }
+
+            public Edge(int weight, int firstVertex, int secondVertex)
+            {
+                Weight = weight;
+                FirstVertex = firstVertex;
+                SecondVertex = secondVertex;
+            }
+        }
+
+        public Graph GetSpanningTree()
+        {
+            var newLengths = new int[CountOfVertexes, CountOfVertexes];
+            var newConnections = new bool[CountOfVertexes, CountOfVertexes];
+
+            var edges = new List<Edge>();
+            var visited = new bool[CountOfVertexes];
+
+            int currentVertex = 0;
+            for (int i = 0; i < CountOfVertexes - 1; ++i)
+            {
+                visited[currentVertex] = true;
+                newConnections[currentVertex, currentVertex] = true;
+
+                for (int j = 0; j < CountOfVertexes; ++j)
+                {
+                    if (!visited[j] && MatrixOfConnections[currentVertex, j])
+                    {
+                        var newEdge = new Edge(MatrixOfLengths[currentVertex, j], Math.Min(currentVertex, j), Math.Max(currentVertex, j));
+                        edges.Add(newEdge);
+                    }
+                }
+
+                Edge maxEdge = null;
+                foreach (var edge in edges)
+                {
+                    if ((!visited[edge.FirstVertex] || !visited[edge.SecondVertex]) && (maxEdge == null ||edge.Weight < maxEdge.Weight))
+                    {
+                        maxEdge = edge;
+                    }
+                }
+
+                if (maxEdge != null)
+                {
+                    edges.Remove(maxEdge);
+
+                    newLengths[maxEdge.FirstVertex, maxEdge.SecondVertex] = maxEdge.Weight;
+                    newLengths[maxEdge.SecondVertex, maxEdge.FirstVertex] = maxEdge.Weight;
+
+                    newConnections[maxEdge.FirstVertex, maxEdge.SecondVertex] = true;
+                    newConnections[maxEdge.SecondVertex, maxEdge.FirstVertex] = true;
+
+                    currentVertex = visited[maxEdge.FirstVertex] ? maxEdge.SecondVertex : maxEdge.FirstVertex;
+                }
+            }
+
+            return new Graph(newLengths, newConnections);
+        }
+    }
+}
---
+++ Homeworks/Homework_5/Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] lengths = { { 0, 78, 0, 0, 16 },
+                               { 78, 0, 17, 22, 6 },
+                               { 0, 17, 0, 30, 0 },
+                               { 0, 22, 30, 0, 29 },
+                               { 16, 6, 0, 29, 0 } };
+            bool[,] connections = { { true, true, false, false, true },
+                                    { true, true, true, true, true },
+                                    { false, true, true, true, false },
+                                    { false, true, true, true, true },
+                                    { true, true, false, true, true } };
+
+            var graph = new Graph(lengths, connections);
+            var tree = graph.GetSpanningTree();
+            bool a = graph.IsConnected();
+        }
+    }
+}
