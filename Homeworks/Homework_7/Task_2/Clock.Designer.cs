1e49dd3dda4b3fe69c4ad0fd9b491863f6db2cbf
AnatoliyZaginai <arkey7724@gmail.com>
2021-05-21 13:43:45 +0300

    Added clock form and comments.

---
+++ Homeworks/Homework_7/Task_2/Clock.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Task_2
+{
+    partial class Clock
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(482, 453);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxPaint);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timerTick);
+            // 
+            // Clock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.Controls.Add(this.pictureBox);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Clock";
+            this.Text = "Clock";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Homeworks/Homework_7/Task_2/Clock.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Task_2
+{
+    /// <summary>
+    /// Clock form class.
+    /// </summary>
+    public partial class Clock : Form
+    {
+        /// <summary>
+        /// Creates a new clock form.
+        /// </summary>
+        public Clock()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Constants that determine the length of the hands relative to the form size.
+        /// </summary>
+        private const float secondHandRelativeLength = 0.4f;
+        private const float minuteHandRelativeLength = 0.3f;
+        private const float hourHandRelativeLength = 0.2f;
+
+        /// <summary>
+        /// Constants that determine the width of the hands relative to the form size.
+        /// </summary>
+        private const float secondHandRelativeWidth = 0.005f;
+        private const float minuteHandRelativeWidth = 0.01f;
+        private const float hourHandRelativeWidth = 0.02f;
+
+        /// <summary>
+        /// Сonstant that determines the width of the clock face relative to the form size.
+        /// </summary>
+        private const float clockFaceRelativeWidth = 0.01f;
+
+        /// <summary>
+        /// Сonstant that determines the width of the clock face font relative to the form size.
+        /// </summary>
+        private const float fontRelativeSize = 0.0035f;
+
+        private Matrix RotateAroundPoint(float angle, PointF center)
+        {
+            var result = new Matrix();
+            result.RotateAt(angle, center);
+            return result;
+        }
+
+        private void DrawHand(Graphics graphics, Color color, float angle, float relativeLength, float relativeWidth)
+        {
+            var size = Math.Min(pictureBox.Height, pictureBox.Width);
+            var pen = new Pen(color, relativeWidth * size);
+            PointF center = new(pictureBox.Width / 2f, pictureBox.Height / 2f);
+            var length = size * relativeLength;
+
+            graphics.Transform = RotateAroundPoint(angle, center);
+            PointF newPoint = new(pictureBox.Width / 2f, pictureBox.Height / 2f - length);
+            graphics.DrawLine(pen, center, newPoint);
+        }
+
+        private void DrawClockFace(Graphics graphics, Color color, float clockFaceRelativeWidth, float fontRelativeSize)
+        {
+            var size = Math.Min(pictureBox.Height, pictureBox.Width) * 0.95f;
+            var pen = new Pen(color, clockFaceRelativeWidth * size);
+            PointF center = new(pictureBox.Width / 2f, pictureBox.Height / 2f);
+
+            graphics.DrawEllipse(pen, center.X - size / 2f, center.Y - size / 2f, size, size);
+
+            for (int i = 1; i <= 12; ++i)
+            {
+                graphics.Transform = RotateAroundPoint(30 * i, center);
+                PointF newPoint = new(center.X, center.Y - size * 0.9f / 2f);
+                graphics.TranslateTransform(newPoint.X, newPoint.Y);
+                graphics.RotateTransform(-30 * i);
+                var format = new StringFormat();
+                format.LineAlignment = StringAlignment.Center;
+                format.Alignment = StringAlignment.Center;
+                graphics.DrawString($"{i}", new Font("Arial", 12 * size * fontRelativeSize), Brushes.Black, 0, 0, format);
+            }
+        }
+
+        private void DrawClock(Graphics graphics)
+        {
+            DrawClockFace(graphics, Color.Black, clockFaceRelativeWidth, fontRelativeSize);
+            DrawHand(graphics, Color.Red, 6f * DateTime.Now.Second, secondHandRelativeLength, secondHandRelativeWidth);
+            DrawHand(graphics, Color.Black, 6f * (DateTime.Now.Minute + DateTime.Now.Second / 60f), minuteHandRelativeLength, minuteHandRelativeWidth);
+            DrawHand(graphics, Color.Black, 30f * (DateTime.Now.Hour + DateTime.Now.Minute / 60f + DateTime.Now.Second / 3600f), hourHandRelativeLength, hourHandRelativeWidth);
+        }
+
+        private void pictureBoxPaint(object sender, PaintEventArgs e)
+        {
+            DrawClock(e.Graphics);
+        }
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            pictureBox.Invalidate();
+        }
+    }
+}
