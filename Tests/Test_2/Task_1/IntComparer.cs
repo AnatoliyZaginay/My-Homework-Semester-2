b83e74effe77db92999c6d37a9aa38e4a3e35098
AnatoliyZaginai <arkey7724@gmail.com>
2021-05-28 12:47:17 +0300

    Added task.

---
+++ Tests/Test_2/Task_1/IntComparer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_1
+{
+    /// <summary>
+    /// Class for comparing integers.
+    /// </summary>
+    public class IntComparer : IComparer<int>
+    {
+        /// <summary>
+        /// Returns the difference between first number and second number.
+        /// </summary>
+        /// <param name="x">First number.</param>
+        /// <param name="y">Second number.</param>
+        public int Compare(int x, int y)
+            => x - y;
+    }
+}
